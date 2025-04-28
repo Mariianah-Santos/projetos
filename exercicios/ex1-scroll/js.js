377baad0843491072c1642ece973e2aeb764bc58
Mariianah-Santos <mariana20121971@hotmail.com>
2025-04-28 11:20:46 -0300

    first commit

---
+++ exercicios/ex1-scroll/js.js
@@ -0,0 +1,8 @@
+function right() {
+    const scroll7 = document.getElementById("scroll7");
+    scroll7.scrollBy({ left: 100, behavior: 'smooth'});
+}
+function left() {
+    const scroll7 = document.getElementById("scroll7");
+    scroll7.scrollBy({ left: - 100, behavior: 'smooth'});
+}
